ce0a8add6bd7be458456d18cd3aecf07a75bc851
devbaoo_712 <caokhacbao0712@gmail.com>
2024-10-24 22:33:05 +0700

    first commit

---
+++ Presentation/Program.cs
@@ -0,0 +1,32 @@
+using Application.Services.Implementations;
+using Application.Services.Interfaces;
+using Data.UnitOfWork.Implementations;
+using Data.UnitOfWorks.Interfaces;
+using Domain.Context;
+using Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var sqlConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BilliardsContext>(options => options.UseSqlServer(sqlConnectionString));
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDependencyInjection();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
